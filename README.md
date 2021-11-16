b027b7760df91359ec1c921154b0ad166eec7540
Tolga Sayan <tolgasyn2@gmail.com>
2021-11-16 19:29:33 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Welcome page, Login Page and Sign up page - Flutter UI
+
+
+
+
+
+- flutter_svg: [link](https://pub.dev/packages/flutter_svg)
+
+
+
---
+++ lib/Screens/Welcome/components/body.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Login/login_screen.dart';
+import 'package:flutter_auth/Screens/Signup/signup_screen.dart';
+import 'package:flutter_auth/Screens/Welcome/components/background.dart';
+import 'package:flutter_auth/components/rounded_button.dart';
+
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    // This size provide us total height and width of our screen
+    return Background(
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "FAME",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: size.height * 0.05),
+
+            SizedBox(height: size.height * 0.05),
+            RoundedButton(
+              text: "Giriş",
+              color: Colors.deepPurpleAccent,
+              textColor: Colors.white,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+            RoundedButton(
+              text: "Kayıt Ol",
+              color: Colors.deepPurpleAccent,
+              textColor: Colors.white,
+              press: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return SignUpScreen();
+                    },
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
